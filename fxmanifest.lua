4da93fd78441d8068bf668e0c2995ea5f214caac
Kevin <163057317+Katania91@users.noreply.github.com>
2024-11-08 16:02:52 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes' -- Enables support for Lua 5.4
+
+author 'Katania'
+description 'A client-side script that disables the helmet for players when riding a motorcycle'
+version '1.0.0'
+
+client_script 'client.lua'
